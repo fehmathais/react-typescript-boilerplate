fbbb418b31971e3d23b2b07e9c7cefeb6e6bb536
Felipe Mathais <fmathais@gmail.com>
2020-04-23 18:22:15 -0300

    Add node-sass package with sass modules

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import './index.css';
+import styles from'./index.module.scss';
 
 export default class Home extends Component<any, any> {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
+			<div className={styles.app}>
+				<header className={styles.header}>
+					<img src={logo} className={styles.logo} alt="logo" />
 					<p>
 						Edit <code>src/App.tsx</code> and save to reload.
 					</p>
 
-					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					<a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 						Learn React
 					</a>
 				</header>
